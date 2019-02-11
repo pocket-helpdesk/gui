09a1b9375575a6126067dcbcd4d4c9c9d6c89c75
Carlos Bravo <Carlos@Karinas-MacBook-Pro.local>
2019-02-10 20:56:55 -0600

    Buttons send template to notes

--- PocketHelpDesk.py
+++ PocketHelpDesk.py
@@ -1,9 +1,19 @@
 #appJar UI Library Import
 from appJar import gui
 
+#Get The user name entered
 def get_user_name():
     user_name = PocketHelpDeskUI.getEntry("User")
     return user_name
+
+#Get Company name entered
+def get_company_name():
+    company_name = PocketHelpDeskUI.getEntry("Company")
+    return company_name
+
+def get_ticket_number():
+    ticket_number = PocketHelpDeskUI.getEntry("Ticket Number")
+    return ticket_number
 
 #Popup that confirms a button working
 def buttontest(buttonmsg):
@@ -12,14 +22,30 @@ def buttontest(buttonmsg):
 
 #Sending the text template to the notes section
 def new_call_default():
-    new_call_text = "{} called in because  .".format(usr)
-    PocketHelpDeskUI.setTextArea("Notes",new_call_text)  
- #   buttontest("Button")
+    new_call_text = "{} called in because  .".format(get_user_name())
+    button = PocketHelpDeskUI.setTextArea("Notes",new_call_text)
+    return button
 
-def called_no_answer(get_user_name):
-    no_answer_text = 'Called {} at ###. No Answer, left a voice mail, sending mail.'.format(get_user_name)
+def called_no_answer():
+    no_answer_text = 'Called {} at ###. No Answer, left a voice mail, sending mail.'.format(get_user_name())
     button = PocketHelpDeskUI.setTextArea("Notes", no_answer_text)
     return button
+
+
+#Email template Section
+
+def escalation_Emailer(text, subject, recipient, auto=True):
+    import win32com.client as win32   
+
+    outlook = win32.Dispatch('outlook.application')
+    mail = outlook.CreateItem(0)
+    mail.To = cprice@allcovered.com
+    mail.Subject = "Escalation - {} - {}".format(get_company_name, get_ticket_number)
+    mail.HtmlBody = text
+    if auto:
+        mail.send
+    else:
+        mail.open # or whatever the correct code is
 
 #SButton event
 def press(button):
@@ -47,17 +73,13 @@ def press(button):
     #else:
     #    return
 
-
-#def noteTemplate():
- #   PocketHelpDeskUI.setTextArea("Text Template", firstNoteTemplate)
-
 #Pocket HelpDesk UI
 PocketHelpDeskUI = gui("Pocket HelpDesk")
 #PocketHelpDeskUI.startTabbedFrame("Tabs")
 #PocketHelpDeskUI.startTab(tab_naming())
 #PocketHelpDeskUI.addLabel("L1","Tab 1")
 PocketHelpDeskUI.setSize(400, 650)
-PocketHelpDeskUI.setBg("Blue")
+PocketHelpDeskUI.setBg("Grey")
 PocketHelpDeskUI.setFont(18)
 
 
@@ -73,12 +95,12 @@ PocketHelpDeskUI.addLabelEntry("User")
 
 
 #Email Templete Widget
-#PocketHelpDeskUI.addButton("Emails",buttontest)
+PocketHelpDeskUI.addButton("Emails", escalation_Emailer)
 #PocketHelpDeskUI.setButtonTooltip("Emails", "Pick the email you want to send")
 #PocketHelpDeskUI.enableButtonTooltip("Emails")
 
 #Button Templates for ticket notes
-PocketHelpDeskUI.addButton("New Call", press)
+PocketHelpDeskUI.addButton("New Call", new_call_default)
 PocketHelpDeskUI.addButton("No Answer", called_no_answer)
 
 #CheckBoxes for Templates
@@ -95,7 +117,7 @@ PocketHelpDeskUI.addLabel("Notes")
 PocketHelpDeskUI.addScrolledTextArea("Notes", text = None)
 
 #Link button to function press
-PocketHelpDeskUI.addButtons(["clear", "Cancel"], press)
+PocketHelpDeskUI.addButtons(["Clear", "Cancel"], press)
 #PocketHelpDeskUI.setFocus("Company")
 PocketHelpDeskUI.go()
 
