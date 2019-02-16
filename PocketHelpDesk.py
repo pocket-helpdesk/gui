2b53f8c8ac0cbacd529ef390e2693073501017af
Carlos Bravo <carlos9035@gmail.com>
2019-02-16 00:26:46 -0600

    clear button: Clears all text, saves notes as text file with ticket number as file name.

--- PocketHelpDesk.py
+++ PocketHelpDesk.py
@@ -1,6 +1,6 @@
 #appJar UI Library Import
 from appJar import gui
-
+#Template function are here
 #Get The user name entered
 def get_user_name():
     user_name = PocketHelpDeskUI.getEntry("User")
@@ -19,10 +19,9 @@ def get_ticket_number():
 def buttontest(buttonmsg):
     PocketHelpDeskUI.infoBox("Button testing", "This buttton for "+ buttonmsg + " is working" )
     
-
 #Sending the text template to the notes section
 def new_call_default():
-    new_call_text = "{} called in because  .".format(get_user_name())
+    new_call_text = ("{} called in because needs assistance with \n\n PC Name: PC_NAME\n\n How did you verify? \n Any other users affected? \n When is the last time it worked?\n \n Additional Troubleshooting:".format(get_user_name()))
     button = PocketHelpDeskUI.setTextArea("Notes",new_call_text)
     return button
 
@@ -31,93 +30,77 @@ def called_no_answer():
     button = PocketHelpDeskUI.setTextArea("Notes", no_answer_text)
     return button
 
-
+def pwd_reset(pwd_chosen):
+    if pwd_chosen == "psswd AD":
+        pwd_text_ad = "getting his domain password Reset. Logged in to the DC - \n \n Located the user, reset the password \n \n Was the user able to login? \n"
+        button = PocketHelpDeskUI.setTextArea("Notes", pwd_text_ad)
+    else:
+        pwd_text_365 = "\n getting the passowrd for office 365 reset.\n \n Logged in to O365 Admin console, resetting the password \n \n Are user able to change their own passwords? \n Did you set a password or entered a TEMP so the user can change it? \n Was the user able to Authenticate? \n \n Aditional Information:"
+        button = PocketHelpDeskUI.setTextArea("Notes", pwd_text_365)
+        return button
 #Email template Section
-
-def escalation_Emailer(text, subject, recipient, auto=True):
-    import win32com.client as win32   
-
+def change_mgmnt_email(text, subject, recipient, auto = True):
+    import win32com.client as win32
     outlook = win32.Dispatch('outlook.application')
     mail = outlook.CreateItem(0)
-    mail.To = cprice@allcovered.com
-    mail.Subject = "Escalation - {} - {}".format(get_company_name, get_ticket_number)
+    mail.To = recipient
+    mail.Subject = subject
     mail.HtmlBody = text
     if auto:
         mail.send
     else:
-        mail.open # or whatever the correct code is
+        mail.Display(False)
+
+    
+     
+
+def save_to_text():
+    notes_text = PocketHelpDeskUI.getTextArea("Notes")
+    file = open("./tickets/{}.txt".format(get_ticket_number()),"w")
+    file.write(notes_text)
+    file.close()
 
 #SButton event
 def press(button):
-    if button == "Cancel":
-        PocketHelpDeskUI.stop()
+    if button == "Clear":
+        PocketHelpDeskUI.clearTextArea("Notes")
     else:
-        pressed_button = button
-        button = PocketHelpDeskUI.setTextArea("Notes", button)
-        return button
+        PocketHelpDeskUI.stop()
 
-#Sending the text on the checked boxes
-#def checkBoxTemplate(cbt):
-#    checkedtext = "This is the text that goes on the notes after Checking the Box."
-#    PocketHelpDeskUI.setTextArea("Notes", checkedtext)
-#    buttontest("Checked")
-
-#def get(btn):
-#    tickdict = PocketHelpDeskUI.getOptionBox("Task")
-#      checkBoxTemplate("cbt")
-    
-    #if tickdict['template1']:
-    #     firstNoteTemplate("x")
-    #elif tickdict["template2"]:
-    #    buttontest("tick options")
-    #else:
-    #    return
 
 #Pocket HelpDesk UI
 PocketHelpDeskUI = gui("Pocket HelpDesk")
-#PocketHelpDeskUI.startTabbedFrame("Tabs")
-#PocketHelpDeskUI.startTab(tab_naming())
-#PocketHelpDeskUI.addLabel("L1","Tab 1")
-PocketHelpDeskUI.setSize(400, 650)
+PocketHelpDeskUI.setSize(500, 650)
 PocketHelpDeskUI.setBg("Grey")
-PocketHelpDeskUI.setFont(18)
-
+PocketHelpDeskUI.setFont(12)
+PocketHelpDeskUI.setButtonFont(size = 10, family = 'times', underline = True)
+PocketHelpDeskUI.setOnTop(stay=True)
 
 #Widgets
 #Ticket Entry
-PocketHelpDeskUI.addLabelEntry("Ticket Number")
+PocketHelpDeskUI.addLabelEntry("Ticket Number",0, 0)
 
 #Company name Entry
-PocketHelpDeskUI.addLabelEntry("Company")
+PocketHelpDeskUI.addLabelEntry("Company",1, 4)
 
 #User Entry
-PocketHelpDeskUI.addLabelEntry("User")
-
+PocketHelpDeskUI.addLabelEntry("User",2, 4)
 
 #Email Templete Widget
-PocketHelpDeskUI.addButton("Emails", escalation_Emailer)
-#PocketHelpDeskUI.setButtonTooltip("Emails", "Pick the email you want to send")
-#PocketHelpDeskUI.enableButtonTooltip("Emails")
+PocketHelpDeskUI.addButton("Emails", change_mgmnt_email,3,0)
 
 #Button Templates for ticket notes
-PocketHelpDeskUI.addButton("New Call", new_call_default)
+PocketHelpDeskUI.addButton("New Call", new_call_default,3,4)
+
+
 PocketHelpDeskUI.addButton("No Answer", called_no_answer)
-
-#CheckBoxes for Templates
-#PocketHelpDeskUI.addCheckBox("Text Template 2 ")
-
-
-#Tick Options
-#PocketHelpDeskUI.addLabelOptionBox("Task", ["template1","template2","template3"])
-#PocketHelpDeskUI.addButton("Get", get)
-
-#PocketHelpDeskUI.addButton("Get", buttontest)
+PocketHelpDeskUI.addButtons(["psswd AD","psswd 365"], pwd_reset)
 #Notes Widget
 PocketHelpDeskUI.addLabel("Notes")
-PocketHelpDeskUI.addScrolledTextArea("Notes", text = None)
-
+PocketHelpDeskUI.addScrolledTextArea("Notes", text = None, colspan=6)
+#ocketHelpDeskUI.searchTextArea("Search",start=None, stop=None, nocase=True, backwards=False)
 #Link button to function press
-PocketHelpDeskUI.addButtons(["Clear", "Cancel"], press)
+PocketHelpDeskUI.addButtons(["Clear", "Cancel"], press, colspan=6)
 #PocketHelpDeskUI.setFocus("Company")
 PocketHelpDeskUI.go()
 
